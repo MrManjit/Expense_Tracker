371f0aa308b2f7d84b6b9eeb5c5134f2f6aa8850
Manjit Singh <mj.ahlawat14@gmail.com>
2025-12-22 12:00:12 +0530

    test9

--- Expense_Tracker/urls.py
+++ Expense_Tracker/urls.py
@@ -24,11 +24,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),  # login/logout/password views
     path("", include("ledger.urls")),
-    path("", RedirectView.as_view(pattern_name="dashboard", permanent=False)),
 ]
