a09003e5c0c23ad6b17eaa3d38c471c12aba34b5
Manjit Singh <mj.ahlawat14@gmail.com>
2025-12-22 14:42:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,9 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+echo "Creating superuser if not exists........."
+python manage.py createsuperuser --noinput || true
+
 echo "Starting Gunicorn..."
 exec gunicorn Expense_Tracker.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
